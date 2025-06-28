272e6d67cbcaebd24e2a36799393cc3aa8260f00
Jim Myhrberg <contact@jimeh.me>
2025-06-28 21:21:05 +0100

    feat: initial commit of EmacsLG icons

---
+++ EmacsLG/EmacsLG/EmacsLGApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmacsLGApp.swift
+//  EmacsLG
+//
+//  Created by jimeh on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EmacsLGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
